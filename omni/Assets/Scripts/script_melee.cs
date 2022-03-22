07cb4191ad0944a5c04b96143beaa35a92dd969f
Venyxx <scottvanessa01@gmail.com>
2022-03-22 18:19:59 -0400

    18th Commit
    SWWWOAARD

--- omni/Assets/Scripts/script_melee.cs
+++ omni/Assets/Scripts/script_melee.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class script_melee : MonoBehaviour
 {
     // Start is called before the first frame update
+    public script_weapon_swap weaponSwapAccess;
+    public TextMeshProUGUI swingGUI;
     void Start()
     {
         
@@ -13,6 +16,27 @@ public class script_melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (weaponSwapAccess.sword == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Swing ();
+            Invoke ("UnSwing", 2f);
+        }
+        }
     }
+
+    void Swing ()
+    {
+        //animation
+        //collider
+        swingGUI.text = "SWING";
+    }
+
+    void UnSwing ()
+    {
+        swingGUI.text = "";
+    }
+
+
 }
