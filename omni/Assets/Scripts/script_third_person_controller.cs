5080bf95dea1856cca1512f97959ce4e32420b49
Venyxx <scottvanessa01@gmail.com>
2022-03-30 14:41:28 -0400

    Character Controller
    changing him

---
+++ omni/Assets/Scripts/script_third_person_controller.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class script_third_person_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+    private StarterAssetsInputs starterAssetsInputs;
+    private ThirdPersonController thirdPersonController;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+
+    private void Awake()
+
+    {
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
+    }
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            //DebugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+
+        }
+
+        if (starterAssetsInputs.Aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
+            thirdPersonController.SetRotateOnMove(false);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+
+            transform.forward = Vector3.Lerp (transform.forward, aimDirection, Time.deltaTime * 20f);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+            thirdPersonController.SetRotateOnMove(true);
+        }
+
+
+    }
+}
