31c56067fcdc47c8ccb506f2e8871ff5395f590b
Venyxx <scottvanessa01@gmail.com>
2022-04-14 23:09:11 -0400

    boss room swap

--- omni/Assets/Scripts/boss_encounter/scipt_boss_control.cs
+++ omni/Assets/Scripts/boss_encounter/scipt_boss_control.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class scipt_boss_control : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool favorsDark;
     public bool favorsLight;
-    private float LDTimerMax = 10f;
+    public float LDTimerMax = 10f;
 
     private float LDTimerCurrent;
     public Material skybox_light;
     public Material skybox_dark;
+    public TextMeshProUGUI favorsGUI;
+    
 
     void Start()
     {
         RenderSettings.skybox = skybox_light;
         LDTimerCurrent = LDTimerMax;
         favorsLight = true;
+        favorsGUI.text = "";
     }
 
     // Update is called once per frame
@@ -29,15 +33,22 @@ public class scipt_boss_control : MonoBehaviour
             //boss regains favor of dark
             RenderSettings.skybox = skybox_dark;
             LDTimerCurrent = LDTimerMax;
-
+            favorsLight = false;
+            favorsDark = true;
+            favorsGUI.text = "___ regains favor of the Dark";
             Debug.Log("Switching to dark");
+            Invoke ("guiWaitSpace", 3f);
         }
         else if (LDTimerCurrent < 0 && favorsDark)
         {
             //regains favor of light;
             RenderSettings.skybox = skybox_light;
             LDTimerCurrent = LDTimerMax;
+            favorsDark = false;
+            favorsLight = true;
+            favorsGUI.text = "____regains favor of the Light";
             Debug.Log("Switching to light");
+            Invoke ("guiWaitSpace", 3f);
         }
     }
 
@@ -48,4 +59,10 @@ public class scipt_boss_control : MonoBehaviour
             LDTimerCurrent -= Time.deltaTime; 
         }
     }
+     
+    void guiWaitSpace ()
+    {
+        favorsGUI.text = "";
+    }
+   
 }
