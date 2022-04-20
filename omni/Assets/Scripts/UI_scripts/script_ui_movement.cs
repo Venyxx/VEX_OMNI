afbdd791bda286d70d4ca797a77b11363fb470d8
Venyxx <scottvanessa01@gmail.com>
2022-04-20 17:16:37 -0400

    uiUD

--- omni/Assets/Scripts/UI_scripts/script_ui_movement.cs
+++ omni/Assets/Scripts/UI_scripts/script_ui_movement.cs
@@ -6,27 +6,70 @@ public class script_ui_movement : MonoBehaviour
 {
     public GameObject leftMost;
     public GameObject rightMost;
-    public Transform center;
+    //public Transform center;
     public Transform leftStart;
     public Transform rightStart;
 
     public float leftLocation;
     public float rightLocation;
+    Transform leftPos;
+    Transform rightPos;
 
-    public script_lantern lanternAccess;
+    public script_character_movement characterAccess;
+    GameObject player;
+    Vector2 screenCenterPoint;
+    Vector3 leftPosStart;
+    public bool changeUp = false;
+    public bool changeDown = false;
+    Vector3 updating = new Vector3(81.5f,0,0);
 
     // Start is called before the first frame update
     void Start()
     {
-        Transform leftPos = leftMost.GetComponent<Transform>();
-        Transform rightPos = rightMost.GetComponent<Transform>();
-        GameObject lanternOne = GameObject.Find("first_lantern_DONOTRENAME");
-        lanternAccess = lanternOne.GetComponent<script_lantern>();
+         leftPos = leftMost.GetComponent<Transform>();
+         rightPos = rightMost.GetComponent<Transform>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        characterAccess = player.GetComponent<script_character_movement>();
+        //screenCenterPoint = new Vector3(Screen.width / 2f, Screen.height / 2f,0);
+       //  leftPosStart = new Vector3(leftPos.transform.position.x, leftPos.transform.position.y,0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Vector2 leftPosVector = new Vector3(0,0,0);
+        
+       // leftPosVector.x = leftPos.transform.position.x;
+        //leftPosVector.y = leftPos.transform.position.y;
+        
+       // Debug.Log("screen center" + screenCenterPoint + "left pos" + leftPosVector);
 
+        if(changeUp)
+        {
+            Increase();
+        }
+
+        if (changeDown)
+        {
+            Decrease();
+        }
+        
+
+       
+
+    }
+
+    void Increase ()
+    {
+        leftPos.transform.position = Vector2.Lerp(leftPos.transform.position, leftPos.transform.position + updating,  20f );
+        rightPos.transform.position = Vector2.Lerp(rightPos.transform.position, rightPos.transform.position - updating,  20f);
+        changeUp = false;
+    }
+
+    void Decrease ()
+    {
+        leftPos.transform.position = Vector2.Lerp(leftPos.transform.position, leftPos.transform.position - updating,  20f);
+        rightPos.transform.position = Vector2.Lerp(rightPos.transform.position, rightPos.transform.position + updating,  20f);
+        changeDown = false;
     }
 }
