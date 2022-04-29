6ba727037c15717c28e725a9acba11ef99936f8c
warlordchieftain <89570269+warlordchieftain@users.noreply.github.com>
2022-04-29 03:04:01 -0400

    solsticecams

--- omni/Assets/script_play_lines.cs
+++ omni/Assets/script_play_lines.cs
@@ -8,6 +8,10 @@ public class script_play_lines : MonoBehaviour
     public AudioSource audioSource;
     public float lengthOfClip;
     public bool playing;
+    public GameObject cam1;
+    public GameObject brilltimer;
+    public GameObject dolly2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,9 @@ public class script_play_lines : MonoBehaviour
     {
         if (other.tag == "Player" && playing == false)
         {
+            cam1.SetActive(true);
+            dolly2.SetActive(true);
+            brilltimer.SetActive(false);
             playing = true;
             audioSource.PlayOneShot(audioClip, 10f);
             Invoke("waitSpace", lengthOfClip);
@@ -26,6 +33,9 @@ public class script_play_lines : MonoBehaviour
 
     void waitSpace()
     {
+        cam1.SetActive(false);
+        dolly2.SetActive(false);
+        brilltimer.SetActive(true);
         Destroy(gameObject);
     }
 }
