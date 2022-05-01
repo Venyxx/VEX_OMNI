a24d06d48fe4fb8cc98449220add29b0bad1d377
spicepeach <89557364+spicepeach@users.noreply.github.com>
2022-05-01 01:19:56 -0400

    Small Changes
    adding textures, fixed boss level to load win screen after cutscene

--- omni/Assets/JaguarJak.cs
+++ omni/Assets/JaguarJak.cs
@@ -10,42 +10,40 @@ public class JaguarJak : MonoBehaviour
     public GameObject finalTimeline;
     public GameObject thatBoy;
     public GameObject gazestop;
+
     [SerializeField] GameObject boss;
     scipt_boss_control scipt_boss_control;
     public Material finalsky;
     public GameObject postP;
+
     [SerializeField] GameObject musicCam;
-public GameObject jaguars;
-public GameObject lunardistortion;
+    public GameObject jaguars;
+    public GameObject lunardistortion;
 
-   // void Start()
-   // /{
-         // StartCoroutine(ActivationRoutine());
-   // }
 
-void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-    
-            borealis.SetActive(false);
-            finalCam.SetActive(true);
-            finalTimeline.SetActive(true);
-            thatBoy.SetActive(true);
-    gazestop.SetActive(false);
-     boss.GetComponent<scipt_boss_control>().enabled = false;
-     musicCam.GetComponent<AudioSource>().enabled = false;
-    RenderSettings.skybox = finalsky;
-    postP.SetActive(false);
-    jaguars.SetActive(true);
-    lunardistortion.SetActive(true);
 
-    
-}
-/*private IEnumerator ActivationRoutine()
+        StartCoroutine(ActivationRoutine());
+
+        borealis.SetActive(false);
+        finalCam.SetActive(true);
+        finalTimeline.SetActive(true);
+        thatBoy.SetActive(true);
+        gazestop.SetActive(false);
+        boss.GetComponent<scipt_boss_control>().enabled = false;
+        musicCam.GetComponent<AudioSource>().enabled = false;
+        RenderSettings.skybox = finalsky;
+        postP.SetActive(false);
+        jaguars.SetActive(true);
+        lunardistortion.SetActive(true);
+    }
+    private IEnumerator ActivationRoutine()
      {  
          //Turn the Game Oject back off after x sec.
          yield return new WaitForSeconds(32);
  
         SceneManager.LoadScene("WinGame");
         
-     }*/
+     }
 }
